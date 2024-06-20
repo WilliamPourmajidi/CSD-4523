1c15b1eb3fb408730a155f46b6ef30b0633d1de1
William Pourmajidi <William.Pourmajidi@gmail.com>
2024-06-20 14:27:15 -0400

    Improve the example for timer by adding a new function called orient.

--- Lecture-6-Timer.py
+++ Lecture-6-Timer.py
@@ -1,9 +1,18 @@
 import threading
 
+
 def greet():
-    print("Hello, World!")
+    print("Welcome to Lambton College!")
+
+def orient():
+    print("Welcome to Python programming")
+
 
 # Create a timer that executes the greet function after 5 seconds
-timer = threading.Timer(5.0, greet)
-timer.start()  # Starts the timer
+timer_1 = threading.Timer(5.0, greet)
+timer_1.start()  # Starts the timer
 
+timer_2 = threading.Timer(8.0, orient)
+timer_2.start()  # Starts the timer
+
+print("timer has started")
