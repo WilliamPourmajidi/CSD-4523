1c3e06ec7460ec29ca04f6f05d8c20a93bc0d108
William Pourmajidi <William.Pourmajidi@gmail.com>
2024-08-08 17:16:50 -0400

    Lecture contents are added

--- helloworld_project/hello/views.py
+++ helloworld_project/hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 def hello_world(request):
     # This function will handle the HTTP request and return a response.
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World! I am coming to your from the world of Django!!!")
